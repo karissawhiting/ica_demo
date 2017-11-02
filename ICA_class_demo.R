afd9647b36a256ca6f9f2cc9717f7e0b7de07427
Karissa <whiting@ecohealthalliance.org>
2017-11-02 13:02:34 -0400

    final repo changes before pres

--- ICA_class_demo.R
+++ ICA_class_demo.R
@@ -6,31 +6,34 @@ setwd("~/Repositories/ica_demo")
 #####################################
 #Import Sound Data
 
-#set app to play waves
+#set app to play waves- #mac
+
 setWavPlayer("afplay")
 
 #read in .wav files and subset so they are the same size
-S1 <- readWave("./hp_meg_trim.wav") #44100
-S1 <- S1[1:114624]
-S2 <- readWave("./snap.wav")
-S2 <- S2[1:114624]
-S3 <- readWave("./karissa.wav")
-S3 <- S3[1:114624]
+S1 <- readWave("./snapping.wav") #44100
+S1 <- S1[1:474235]
+S2 <- readWave("./hp_trim2.wav")
+S2 <- S2[1:474235]
+S3 <- readWave("./whistl_trim.wav")
+S3 <- S3[1:474235]
+
 
 #play audio files
 play(S1)
 play(S2)
 play(S3)
 
-Stest<- S1/3
-play(Stest)
+#makes it quieter
+#Stest<- S1/5
+#play(Stest)
 
 #plot right channel input
 #plot(S1@right)
 
 #create white noise file same duration as the rest of the clips
 set.seed(321)
-NOISE <- noise("white", duration = 114624)
+NOISE <- noise("white", duration = 474235)
 play(NOISE)
 
 #bind all the samples together in data frame
@@ -78,25 +81,28 @@ play(x4)
 # which approximates the inverse of the mixing matrix, A−1
 # The objective of ICA is therefore to calculate W, knowing only X
 
-#JADE - uses fourth order moments (kurtosis) to measure non-guassianity. (proxy for independence)
-#JADE assumption Gaussian distributions possess zero excess kurtosis,seeks an orthogonal rotation of the observed mixed vectors to estimate source vectors which possess high values of excess kurtosis.
-#SOBI - joint diagonalization of whole set of autocovariance matricies
+#JADE 
+#uses fourth order moments (kurtosis) to measure non-guassianity. (proxy for independence)
+#JADE assumption Gaussian distributions possess zero excess kurtosis,
+#seeks an orthogonal rotation of the observed mixed vectors to estimate source vectors 
+#which possess high values of excess kurtosis.
+
+#SOBI 
+#joint diagonalization of whole set of autocovariance matricies
+
 jade <- JADE(X)
 sobi <- SOBI(Xt)
-
-#nsstdjd <- NSS.TD.JD(Xt)
-#nsstdjd2 <- NSS.TD.JD(Xt)
 
 #performance indicies
 #Minimum Distance Index- compares the estimated unmixing matrix to the true A matrix
 MD(coef(jade), A)
 MD(coef(sobi), A)
-#MD(coef(nsstdjd), A)
-#MD(coef(nsstdjd2), A)
 
+#extract estimated sources
 Z.nsstdjd <- bss.components(jade)
 Z.nsstdjd <- bss.components(sobi)
 
+#normalize to listen
 NSSTDJDwave1 <- normalize(Wave(left = as.numeric(Z.nsstdjd[, 1]),samp.rate = 44100, bit = 16), unit = "8")
 NSSTDJDwave2 <- normalize(Wave(left = as.numeric(Z.nsstdjd[, 2]), samp.rate = 44100, bit = 16), unit = "8")
 NSSTDJDwave3 <- normalize(Wave(left = as.numeric(Z.nsstdjd[, 3]), samp.rate = 44100, bit = 16), unit = "8")
@@ -106,18 +112,5 @@ play(NSSTDJDwave2)
 play(NSSTDJDwave3)
 play(NSSTDJDwave4)
 
-plot(Z.nsstdjd[,1])
-plot(Z.nsstdjd[,2])
-plot(Z.nsstdjd[,3])
-plot(Z.nsstdjd[,4])
 
-Z.nsstdjd2 <- bss.components(nsstdjd2)
-NSSTDJDwave12 <- normalize(Wave(left = as.numeric(Z.nsstdjd2[, 1]),samp.rate = 44100, bit = 16), unit = "8")
-NSSTDJDwave22 <- normalize(Wave(left = as.numeric(Z.nsstdjd2[, 2]), samp.rate = 44100, bit = 16), unit = "8")
-NSSTDJDwave32 <- normalize(Wave(left = as.numeric(Z.nsstdjd2[, 3]), samp.rate = 44100, bit = 16), unit = "8")
-NSSTDJDwave42 <- normalize(Wave(left = as.numeric(Z.nsstdjd2[, 4]), samp.rate = 44100, bit = 16), unit = "8")
-play(NSSTDJDwave12)
-play(NSSTDJDwave22)
-play(NSSTDJDwave32)
-play(NSSTDJDwave42)
 
